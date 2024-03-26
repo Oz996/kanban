ddcc1f4e3462bc914b9886508b8f7b013a0c0cfa
Oz996 <ozgun996@hotmail.com>
2024-03-26 14:15:34 +0100

    moved provider options to seperate folder

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,6 @@
 import NextAuth from "next-auth";
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { options } from "./options";
 
-const options: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
 const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
