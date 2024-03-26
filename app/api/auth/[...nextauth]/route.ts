0cd4fb9dde98040621cd8daa5134b3513e3c1742
Oz996 <ozgun996@hotmail.com>
2024-03-26 11:54:55 +0100

    moving all next auth logic into route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,17 @@
-import NextAuth from "next-auth/next";
-import { options } from "./options";
+import NextAuth from "next-auth/";
+
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const options: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 export const handler = NextAuth(options);
 
