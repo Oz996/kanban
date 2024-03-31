8568861eb0d7d13878c7e96abe3b4cf9e384658c
Oz996 <ozgun996@hotmail.com>
2024-03-31 12:06:51 +0200

    new landing page

--- app/GoogleAuthButton.tsx
+++ app/GoogleAuthButton.tsx
@@ -3,5 +3,5 @@ import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function GoogleAuthButton() {
-  return <Button onClick={() => signIn("google")}>Sign in</Button>;
+  return <Button onClick={() => signIn("google")}>Sign in with Google</Button>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
   console.log("boards", boards);
   console.log("sesh", session);
 
-  // we route the user to the first board in the fetched list if authenticated
+  // we route the user to the first board in the fetched list of boards if authenticated
   const firstBoardId = boards && boards.length > 0 ? boards[0]?.id : null;
 
   useEffect(() => {
@@ -22,14 +22,25 @@ export default function Page() {
   }, [session, router, firstBoardId]);
 
   return (
-    <section className="h-screen pt-24">
-      <div className="text-white font-semibold flex flex-col justify-center items-center gap-5">
-        <hgroup className="text-center">
-          <h1 className="text-3xl">Welcome to Kanban Task Manager</h1>
-          <p className="text-2xl mt-3">Sign in</p>
-        </hgroup>
-        <GoogleAuthButton />
-        <GuestSignInButton />
+    <section className="h-screen w-screen grid grid-cols-1 md:grid-cols-[60%,40%]">
+      <div className="bg-gradient-to-b from-grey-darker to-grey-dark text-white pt-10 md:pt-44">
+        <h1 className="text-4xl text-center mb-4 font-semibold">
+          Welcome to Kanban Task Manager
+        </h1>
+        <p className="text-lg w-[90%] md:w-[35rem] mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio
+          itaque incidunt nemo officiis, sapiente repudiandae odio accusamus
+          consequatur saepe!
+        </p>
+      </div>
+      <div className="bg-white flex flex-col gap-3 pt-10 md:pt-44">
+        <h2 className="text-center text-grey-darker text-3xl font-bold">
+          Sign in
+        </h2>
+        <div className="w-[90%] sm:w-[20rem] flex flex-col justify-center gap-2 pt-5 mx-auto">
+          <GoogleAuthButton />
+          <GuestSignInButton />
+        </div>
       </div>
     </section>
   );
