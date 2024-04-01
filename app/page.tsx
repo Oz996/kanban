65b5ab965b2c7c6c9f1410567a49eb432aee7539
Oz996 <ozgun996@hotmail.com>
2024-04-01 10:19:29 +0200

    redesigned landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,22 @@
 "use client";
-import { useSession } from "next-auth/react";
 import GoogleAuthButton from "./GoogleAuthButton";
-import { useFetchBoards } from "@/hooks/useFetchBoards";
 import GuestSignInButton from "./GuestSignInButton";
 
 export default function Page() {
-  const { data: session } = useSession();
-  const { data: boards } = useFetchBoards();
-
-  console.log("boards", boards);
-  console.log("sesh", session);
-
   return (
-    <section className="h-screen w-screen grid grid-cols-1 md:grid-cols-[60%,40%]">
-      <div className="bg-gradient-to-b from-grey-darker to-grey-dark text-white pt-10 md:pt-44">
-        <h1 className="text-4xl text-center mb-4 font-semibold">
+    <section className="h-screen w-screen bg-gradient-to-b from-grey-darker to-grey-dark text-white ">
+      <div className="pt-10 md:pt-44">
+        <h1 className="text-2xl sm:text-3xl text-center mb-4 font-semibold">
           Welcome to Kanban Task Manager
         </h1>
-        <p className="text-lg w-[90%] md:w-[35rem] mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae optio
-          itaque incidunt nemo officiis, sapiente repudiandae odio accusamus
-          consequatur saepe!
-        </p>
-      </div>
-      <div className="bg-white flex flex-col gap-3 pt-10 md:pt-44">
-        <h2 className="text-center text-grey-darker text-3xl font-bold">
-          Sign in
-        </h2>
-        <div className="w-[90%] sm:w-[20rem] flex flex-col justify-center gap-2 pt-5 mx-auto">
-          <GoogleAuthButton />
-          <GuestSignInButton />
+        <div className="rounded-lg py-10 mt-8 px-5 bg-grey-darker w-[90%] sm:w-[27rem] mx-auto">
+          <h2 className="text-center text-xl md:text-2xl font-semibold">
+            Sign in
+          </h2>
+          <div className="sm:w-[20rem] flex flex-col justify-center gap-2 pt-5 mx-auto">
+            <GoogleAuthButton />
+            <GuestSignInButton />
+          </div>
         </div>
       </div>
     </section>
